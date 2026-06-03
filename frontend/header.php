96fc7ec8047cee3e4f820b613f9f4fe15ce4a222
ferjimenezzz <leomes103535@gmail.com>
2026-06-03 15:28:24 -0600

    Implementación del lector RFID con ESP32, conección con la base de datos y validación de tag_id registrado

--- frontend/header.php
+++ frontend/header.php
@@ -5,6 +5,10 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
 if (session_status() === PHP_SESSION_NONE) session_start();
 
 // Ruta dinámica relativa al directorio actual
